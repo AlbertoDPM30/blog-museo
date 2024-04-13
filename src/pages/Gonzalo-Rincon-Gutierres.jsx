b1ca03e23d8ba3b1ef859aa28a7f76dfb7405570
AlbertoDPM30 <albertodpm2000@gmail.com>
2024-04-12 21:40:21 -0400

    2do commit

--- src/pages/Gonzalo-Rincon-Gutierres.jsx
+++ src/pages/Gonzalo-Rincon-Gutierres.jsx
@@ -1,3 +1,20 @@
+import { Grid, Typography } from "@mui/material";
 export const GonzaloRinconGutierres = () => {
-  return <>Gonzalo</>;
+  return (
+    <>
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        alignContent="center"
+        wrap="wrap"
+      >
+        <Typography variant="h1" color="initial">
+          Museo Arquelógico Gonzalo Rincon Gutierres
+        </Typography>
+      </Grid>
+    </>
+  );
 };
